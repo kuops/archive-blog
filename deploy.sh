8687ad8a02cf568e9c8e83fd31a0742db7d9c5e5
kuops <opshsy@gmail.com>
2018-04-26 15:37:38 +0000

    deploy website

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git_commit_msg="$(date + %F) deploy website "
 # git push to github https://github.com/kuops/blog-source-code.git
 git add -A
 git commit -m "$git_commit_msg"
-git push origin develop
+git push origin master
 
 # hexo deploy to static website
 hexo clean
