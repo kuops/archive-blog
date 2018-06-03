5476debdffa253dbddda146e84fba71d47caceec
kuops <opshsy@gmail.com>
2018-06-03 16:13:17 +0000

    2018-06-03 deploy website

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 git_commit_msg="$(date +%F) deploy website "
 
 # git push to github https://github.com/kuops/blog-source-code.git
+git config --global user.name "kuops"
+git config --global user.email opshsy@gmail.com
 git add -A
 git commit -m "$git_commit_msg"
 git push origin master
